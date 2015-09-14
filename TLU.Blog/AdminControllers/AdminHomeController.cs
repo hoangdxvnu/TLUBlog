e9b41733e76680faa75e077d765c8ce3abb06a1a
NguyenVanKinh <nguyenvankinh181196@gmail.com>
2015-09-14 14:01:08 +0700

    View Admin

---
+++ TLU.Blog/AdminControllers/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TLU.Blog.AdminControllers
+{
+    public class AdminHomeController : AdminBaseController
+    {
+        // GET: AdminHome
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ TLU.Blog/AdminControllers/AdminLogInController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TLU.Blog.AdminControllers
+{
+    public class AdminLogInController : Controller
+    {
+        // GET: AdminLogIn
+        public ActionResult logIn()
+        {
+            return View();
+        }
+        // POST
+        [HttpPost]
+        public ActionResult logIn(string UserName,string Password)
+        {
+            const string ACCOUNT_ADMIN = "AccountAdmin";
+            if (UserName=="admin"&&Password=="123")
+            {
+                Helpers.BlogCookies.AddCookie("Nguyen Van Kinh", ACCOUNT_ADMIN);
+                return RedirectToAction("Home", "AdminHome");
+            }
+            return View();
+        }
+    }
+}
