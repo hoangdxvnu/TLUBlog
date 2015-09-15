4c9ffaaa0c6c61bb1551f8ea8353c89eb08669e3
NguyenVanKinh <nguyenvankinh181196@gmail.com>
2015-09-15 10:16:36 +0700

    Update

--- TLU.Blog/App_Start/RouteConfig.cs
+++ TLU.Blog/App_Start/RouteConfig.cs
@@ -14,10 +14,29 @@ namespace TLU.Blog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Admin",
+                url: "Admin",
+                defaults: new { controller = "AdminHome", action = "Home"}
+            );
+
+            routes.MapRoute(
+                name: "LogIn",
+                url: "LogIn",
+                defaults: new { controller = "AdminLogIn", action = "LogIn" }
+            );
+
+            routes.MapRoute(
+                name: "Home",
+                url: "Home",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
